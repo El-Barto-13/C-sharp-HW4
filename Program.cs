0ce11416898f002c148e3635019cd0824f108981
Alexis Smirnov <alexis.smirnoff@mail.ru>
2023-04-03 23:19:38 +0300

    вроде все работает, и спер из инета последнюю

--- Program.cs
+++ Program.cs
@@ -2,23 +2,23 @@
 
 // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
 // и возводит число A в натуральную степень B. 
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+// 3,5 -> 243 (3⁵)
+// 2,4 -> 16
 
 /*
-System.Console.Write("Введиче число которое будет возводиться в степерь: ");
+System.Console.Write("Введите число возводимое в степерь: ");
 int number = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введиче степень числа: ");
-int quadNum = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите степень числа: ");
+int degreeNum = Convert.ToInt32(Console.ReadLine());
 
-int result = Square(number, quadNum);
-System.Console.WriteLine($"Результат полученного значения {result}");
+int result = Square(number, degreeNum);
+Console.WriteLine($"Результат полученного значения {result}");
 
-int Square (int number, int quadNum)
+int Square (int number, int degreeNum)
 {
     int Multiplier = number;
 
-    for(int f = 1; f < quadNum; f++)
+    for(int f = 1; f < degreeNum; f++)
     {
         number = number * Multiplier; 
     }
@@ -33,14 +33,30 @@ int Square (int number, int quadNum)
 // 9012 -> 12
 
 /*
-System.Console.Write("Введиче число: ");
+Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int result = CountNumbers(number);
-System.Console.WriteLine($"Результат суммы цифр в числе: {result}");
+Console.WriteLine($"Результат суммы цифр в числе: {result}");
+
+
+// через while
+/*
+int CountNumbers(int num) 
+{
+    int sum = 0;
+    while (num > 0) 
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+*/
 
 // через for
-int CountNumbers(int num) //354 
+/*
+int CountNumbers(int num)
 {
     int sum;
 
@@ -52,16 +68,20 @@ int CountNumbers(int num) //354
 }
  */
 
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1,2,5,7,19 -> [1,2,5,7,19]
+// 6,1,33 -> [6,1,33]
 /*
-// или через while
-int CountNumbers(int num) //354
+int[] randomArray = new int[8];
+
+for (int i = 0; i < 8; i++)
 {
-    int sum = 0;
-    while (num > 0) //->4 -> 5 -> 3
-    {
-        sum = sum + num % 10;
-        num = num / 10;
-    }
-    return sum;
+randomArray[i] = new Random().Next(1,9);
+Console.Write(randomArray[i] + "");
 }
 */
+
+
+
+
